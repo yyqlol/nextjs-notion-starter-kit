9812b1773bfa7fd774bc372e4454b735eacf34cb
yyqlol <91237683+yyqlol@users.noreply.github.com>
2022-08-13 04:23:49 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Yiqi Yan',
-  domain: 'yiqiswork.com',
+  domain: 'https://yiqiswork.com/',
   author: 'Yiqi Yan',
 
   // open graph metadata (optional)
